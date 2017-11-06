e3a8df0f8ee128556d389e30850feeeff83ef26f
Xueqian Wang <quntas.wang@gmail.com>
2017-11-06 13:22:30 -0500

    Update centos_install_deps.sh

--- centos_install_deps.sh
+++ centos_install_deps.sh
@@ -2,26 +2,10 @@
 
 sed -i 's/requiretty/!requiretty/g' /etc/sudoers
 
-echo "Install dependencies"
+echo "Configure docker device mapper"
 
-yum install epel-release -y
-yum install wget git net-tools bind-utils iptables-services bridge-utils bash-completion yum-utils ansible docker -y
-
-echo "Configure docker device marrer"
-DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | head -1 | cut -d':' -f1 )
+DOCKERVG=$( lsscsi | grep 5 | awk '{print $7}' )
 echo "DEVS=${DOCKERVG}" >> /etc/sysconfig/docker-storage-setup
 echo "VG=docker-vg" >> /etc/sysconfig/docker-storage-setup
-
-docker-storage-setup
-if [ $? -eq 0 ]
-then
-   echo "Docker thin pool logical volume created successfully"
-else
-   echo "Error creating logical volume for Docker"
-   exit 3
-fi
-
-systemctl enable docker
-systemctl start docker
 
 echo "Installation finished"
