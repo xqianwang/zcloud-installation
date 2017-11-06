bbba329d39d20e9433b47e29a40f54f526dae961
xqianwang <quntas.wang@gmail.com>
2017-11-06 14:27:05 -0500

    Added script for configuring cloud storage

---
+++ centos-pv-storage-setup.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+echo "INFO: Configuring cloud storage for postgres now."
+
+PG_DISK=$(lsscsi | grep \:6\] | awk '{print $7}')
+
+PARTITION=$(fdisk -l $PG_DISK | grep ${$PG_DISK}1 | awk '{print $1}')
+
+if [ $? -ne 0 && -z $PARTITION ]; then
+  echo -e "n\np\n1\n\n\nt\nc\na\n1\nw" | fdisk $PG_DISK
+else
+  echo "ERROR: Error for configuring storage."
+  exit 6
+fi
+
+mkdir -p /pgsql
+
+mount $PARTITION /pgsql && chown -R 26:26 /pgsql
+
+ID=$(blkid | grep $PARTITION | grep -oP 'UUID="\K[^"]+')
+if [ -z $ID ]; then
+  ID=$(uuidgen)
+fi
+
+echo "UUID=$ID    /pgsql    xfs    defaults,nofail    1    2"
+
+echo "INFO: Configuration finished!"
