dfe7f9ea6540b0f0df958530444527f546c44bad
Xueqian Wang <quntas.wang@gmail.com>
2017-11-07 07:40:26 -0500

    Update centos-pv-storage-setup.sh

--- centos-pv-storage-setup.sh
+++ centos-pv-storage-setup.sh
@@ -4,10 +4,12 @@ echo "INFO: Configuring cloud storage for postgres now."
 
 PG_DISK=$(lsscsi | grep \:6\] | awk '{print $7}')
 
-PARTITION=$(fdisk -l $PG_DISK | grep ${$PG_DISK}1 | awk '{print $1}')
+PARTITION=$(fdisk -l $PG_DISK | grep {$PG_DISK}1 | awk '{print $1}')
 
-if [ $? -ne 0 && -z $PARTITION ]; then
-  echo -e "n\np\n1\n\n\nt\nc\na\n1\nw" | fdisk $PG_DISK
+if [ $? -ne 0 ]; then
+  if [ -z $PARTITION ]; then
+    echo -e "n\np\n1\n\n\nt\nc\na\n1\nw" | fdisk $PG_DISK
+  fi
 else
   echo "ERROR: Error for configuring storage."
   exit 6
