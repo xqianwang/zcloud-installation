ab0d65ec8d63399dd271b63787a4ed66eaa1b2a8
Xueqian Wang <quntas.wang@gmail.com>
2017-11-08 13:09:14 -0500

    Update centos-pv-storage-setup.sh

--- centos-pv-storage-setup.sh
+++ centos-pv-storage-setup.sh
@@ -6,7 +6,7 @@ PG_DISK=$(sudo -i lsscsi | grep \:6\] | awk '{print $7}')
 
 PARTITION=$(sudo fdisk -l $PG_DISK | grep ${PG_DISK}1 | awk '{print $1}')
 
-if [ $? -ne 0 ]; then
+if [ $? -eq 0 ]; then
   if [ -z $PARTITION ]; then
     echo -e "n\np\n1\n\n\nt\nc\na\n1\nw" | fdisk $PG_DISK
   fi
